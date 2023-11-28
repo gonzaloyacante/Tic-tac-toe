547a3237bb9c89b842d76d3beac601c962808532
Gonzalo Yacante <gyacante9@gmail.com>
2023-11-28 21:11:36 +0000

    Separando logica y constantes y agregado de canvas-confetti

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,24 @@
 import { useState } from "react";
+import confetti from "canvas-confetti";
+
+import { TURNS } from "./constants.js";
+import { checkWinner, checkEndGame } from "./logic.js";
+
 import "./App.css";
 
 import { Square } from "./components/Square.jsx";
 import { WinnerModal } from "./components/WinnerModal.jsx";
-
-const TURNS = {
-  X: "✕",
-  O: "○",
-};
-
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
   const [winner, setWinner] = useState(null);
 
-  const checkWinner = (boardToCheck) => {
-    for (const combo of WINNER_COMBOS) {
-      const [a, b, c] = combo;
-      if (
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) {
-        return boardToCheck[a];
-      }
-    }
-    return null;
-  };
-
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
   };
-
-  const checkEndGame = (newBoard) => {
-    return newBoard.every((square) => square !== null)
-  }
 
   const updateBoard = (index) => {
     if (board[index] || winner) return;
@@ -61,9 +32,10 @@ function App() {
 
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
+      confetti();
       setWinner(newWinner);
     } else if (checkEndGame(newBoard)) {
-      setWinner(false)
+      setWinner(false);
     }
   };
 
